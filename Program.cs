0f54c4177e17ee2b1faa37c7118ff4db69712e1d
Faizan Shaikh <faizanroy000777@gmail.com>
2024-06-07 12:30:12 +0530

    Remove weathercast endpoint

--- Program.cs
+++ Program.cs
@@ -11,14 +11,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(); 
 }
 
 app.UseHttpsRedirection();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
